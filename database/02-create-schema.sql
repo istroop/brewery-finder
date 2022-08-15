1b30b74c7fcd549884231c791019fcf85325016f
Rachel <rayray.allgaier@gmail.com>
2022-08-15 15:47:22 -0400

    created sql for brewery table, and populated brewery and user table

--- database/02-create-schema.sql
+++ database/02-create-schema.sql
@@ -5,14 +5,29 @@
 BEGIN;
 
 -- CREATE statements go here
-DROP TABLE IF EXISTS app_user;
+DROP TABLE IF EXISTS brewery;
+DROP TABLE IF EXISTS app_user CASCADE;
 
 CREATE TABLE app_user (
   id SERIAL PRIMARY KEY,
   user_name varchar(32) NOT NULL UNIQUE,
   password varchar(32) NOT NULL,
   role varchar(32),
-  salt varchar(255) NOT NULL
+  birthdate DATE,
+  email_address varchar(100)
+);
+
+CREATE TABLE brewery (
+    id SERIAL PRIMARY KEY,
+    name varchar(255) NOT NULL,
+    brewer int NOT NULL,
+    hours_of_operation varchar(255),
+    phone character varying(20),
+    history varchar(500),
+    image varchar(255),
+    active_status bool,
+    CONSTRAINT brewer_to_user_id_fkey FOREIGN KEY(brewer)
+                     REFERENCES app_user (id)
 );
 
 COMMIT;
--- database/04-insert-seed-data.sql
+++ database/04-insert-seed-data.sql
@@ -5,5 +5,19 @@
 BEGIN;
 
 -- INSERT statements go here
+INSERT INTO app_user
+    (user_name, password, role, birthdate, email_address) VALUES
+('a', 'a', 'brewer', CAST('01/01/1999' as date), 'a@a.com'),
+('b', 'b', 'brewer', CAST('02/02/1998' as date), 'b@b.com'),
+('c', 'c', 'brewer', CAST('03/03/1997' as date), 'c@c.com'),
+('d', 'd', 'brewer', CAST('04/04/1996' as date), 'd@d.com');
+
+INSERT INTO brewery
+    (name, brewer, hours_of_operation, phone, history, active_status) values
+('The Funkatorium', 1, '11AM - 11PM', '(828) 552-3203','Wicked Weed sour & funky beers are served in a rustic pub with a barrel room & a dog-friendly patio.', true),
+('Highland Brewing Company', 2, '2PM - 9PM', '(828) 299-3370', 'Brewery offering free tours of its facilities plus a low-key taproom to sample a pint or flight.', true),
+('Wicked Weed Brewing Pub', 1, '11AM - 11PM', '(828) 575-9599', 'Hip brewery with a tasting room & a restaurant serving high-end, locally sourced New American fare.', true),
+('Ben''s Tune Up', 3, '4PM - 11PM', '(828) 424-7580', 'Trendy brewery & beer garden offering house sakes, craft beers & Japanese-American fusion eats.', true),
+('Asheville Brewing Company', 4, '', '', '', false);
 
 COMMIT;
