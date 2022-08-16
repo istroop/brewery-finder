6f419b36aa69555b33699e36d16bbf21d825a276
Rachel <rayray.allgaier@gmail.com>
2022-08-16 15:26:39 -0400

    made changes to the page flow, and took brewery list off of the home screen

--- src/main/java/com/techelevator/controller/MainPageController.java
+++ src/main/java/com/techelevator/controller/MainPageController.java
@@ -1,30 +1,18 @@
 package com.techelevator.controller;
 
-import com.techelevator.model.dao.BreweryDAO;
-import com.techelevator.model.dao.UserDAO;
-import com.techelevator.model.dto.Brewery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
-
 @Controller
 public class MainPageController {
 
-    private BreweryDAO breweryDAO;
-
     @Autowired
-    public MainPageController(BreweryDAO breweryDAO) {
-        this.breweryDAO = breweryDAO;
+    public MainPageController() {
     }
 
     @RequestMapping("/")
-    public String getMainPage(HttpServletRequest request) {
-
-        List<Brewery> breweries = breweryDAO.getActiveBreweries();
-        request.setAttribute("breweries", breweries);
+    public String getMainPage() {
         return "mainPage";
 
     }
