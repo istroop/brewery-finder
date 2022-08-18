cf7a4665cfe0c7e61770a81e0af18ea24e20883e
Ian Stroop <ianstroop27@gmail.com>
2022-08-18 10:46:51 -0400

    finished initial backend for new brewery. Fixed redirect issue, now redirects to user's homepage

--- src/main/java/com/techelevator/controller/NewBreweryController.java
+++ src/main/java/com/techelevator/controller/NewBreweryController.java
@@ -8,9 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.techelevator.model.dto.User;
@@ -34,8 +32,10 @@ public class NewBreweryController {
     }
 
     @RequestMapping(path="/breweries", method=RequestMethod.POST)
-    public String createBrewery(@Valid @ModelAttribute Brewery brewery,BindingResult result,
-                                RedirectAttributes flash ) {
+    public String createBrewery(@SessionAttribute User currentUser,
+                                @Valid @ModelAttribute Brewery brewery,
+                                BindingResult result,
+                                RedirectAttributes flash) {
         if(result.hasErrors()) {
             flash.addFlashAttribute("brewery", brewery);
             flash.addFlashAttribute(BindingResult.MODEL_KEY_PREFIX + "brewery", result);
@@ -43,7 +43,10 @@ public class NewBreweryController {
         }
 
         breweryDAO.newBrewery(brewery.getBrewer(), brewery.getName(), brewery.getHoursOfOperation(), brewery.getPhone(), brewery.getHistory(), brewery.getImage(), brewery.getAddress(), true);
-        return "mainPage";
+
+        String userName = currentUser.getUserName();
+
+        return "redirect:/users/" + userName;
     }
 
 }
