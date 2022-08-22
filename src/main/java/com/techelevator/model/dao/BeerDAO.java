ee16e6155903e070093667f7ffdcad289bccbd62
Rachel <rayray.allgaier@gmail.com>
2022-08-22 13:54:26 -0400

    fixing override

--- src/main/java/com/techelevator/model/dao/BeerDAO.java
+++ src/main/java/com/techelevator/model/dao/BeerDAO.java
@@ -6,10 +6,11 @@ import java.util.List;
 public interface BeerDAO {
 
     List<Beer> getBeersByBrewery(int brewery_id);
+    List<Beer> getAllBeersByBrewery(int brewery_id);
     Beer getBeerById(int id);
     void updateBeer(int id, String name, String beerType, double abv, String image, String description, boolean activityStatus);
     void addBeer(int breweryId, String name, String image, String description, double abv, String beerType, boolean activityStatus);
     int getNextId();
     void insertImageByBeerId(String fileName, int parseInt);
-    public void makeBeerInactive(int id);
+    void makeBeerInactive(int id);
 }
